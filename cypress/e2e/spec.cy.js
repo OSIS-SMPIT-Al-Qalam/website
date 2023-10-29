6cf979c5b3020100f3b39918b4980a66ce3dec5b
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-10-29 22:05:47 +0800

    switch!: switch to hugo

--- cypress/e2e/spec.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe("My First Test", () => {
-    it("Visits website", () => {
-        cy.visit("http://localhost:3333");
-
-        cy.contains("Hello!");
-    })
-});
